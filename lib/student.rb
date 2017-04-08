14857990bd0ebd5a6b35cc45030dabbea70df9d8
Dan Parry <parryd424@gmail.com>
2017-04-08 15:11:52 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+# require_relative './user.rb'
 
+class Student < User
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(piece_of_knowledge)
+    @knowledge << piece_of_knowledge
+  end
 end
